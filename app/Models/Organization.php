3a03acbadb3e04bc9b92c766e51e3819086950e8
Petropavlovskiy Artiom <artpetropavlovskij@gmail.com>
2026-04-23 17:32:24 +0500

    test image form config

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 
 class Organization extends Model
@@ -65,8 +66,7 @@ class Organization extends Model
     public function getThumbUrlAttribute(): string | null
     {
         if($this->image) {
-            dd(env('FILESYSTEM_DISK'));
-            if (env('FILESYSTEM_DISK') === 'tws3') {
+            if (Config::get('filesystems.default') === 'tws3') {
                 return Storage::disk('tws3')->url($this->thumbnail);
             }
             return asset('storage/' . $this->thumbnail);
